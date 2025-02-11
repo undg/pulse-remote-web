51bc6a6d8aa61a8417994fe6b5f9ce220bd6e5de
undg <und3rdg@gmail.com>
2025-02-11 13:55:44 +0000

    throttle output sliders with optimistic update

--- src/pages/controller-output.tsx
+++ src/pages/controller-output.tsx
@@ -1,45 +1,56 @@
-import { Fragment, useCallback } from 'react'
+import { Fragment } from 'react'
 import { useVolumeStatus } from '../api/use-vol-status'
 import { Layout } from '../components/layout'
 import { VolumeSlider } from '../components/volume-slider'
 import { dict } from '../dict'
-import throttle from 'lodash.throttle'
+import { useThrottledCallback } from '../utils/use-throttled-callback'
+
+const throttleTime = 200
+const vibrateTime = 10
 
 export const ControllerOutput: React.FC = () => {
 	const vol = useVolumeStatus()
 
-	const handleSinkVolumeChange = (name: string, [volume]: number[]) => {
+	// SINK volume control (with optimistic and throttle)
+	const sinkVolume = (name: string, [volume]: number[]) => {
 		navigator.vibrate([10])
 		return vol.setSink(name, volume)
 	}
 
+	const throttledSinkHandler = useThrottledCallback((outputName: string, volume: number[]) => {
+		sinkVolume(outputName, volume).send()
+	}, throttleTime)
+
+	const handleSinkVolumeChange = (outputName: string, volume: number[]) => {
+		sinkVolume(outputName, volume).optimistic()
+		throttledSinkHandler(outputName, volume)
+	}
+
+	// SINK mute toggle
 	const handleSinkMuteToggle = (name: string) => () => {
 		navigator.vibrate([10])
 		vol.toggleSinkMute(name)
 	}
 
-	const handleSinkInputVolumeChange = (id: number, [volume]: number[]) => {
+	// SINK INPUT volume control (with optimistic and throttle)
+	const sinkInputVolume = (id: number, [volume]: number[]) => {
 		navigator.vibrate([10])
 		return vol.setSinkInput(id, volume)
 	}
 
+	const throttledSinkInputHandler = useThrottledCallback((id: number, volume: number[]) => {
+		sinkInputVolume(id, volume).send()
+	}, throttleTime)
+
+	const handleSinkInputVolumeChange = (id: number, volume: number[]) => {
+		sinkInputVolume(id, volume).optimistic()
+		throttledSinkInputHandler(id, volume)
+	}
+
+	// SINK INPUT mute toggle
 	const handleSinkInputMuteToggle = (id: number) => () => {
 		navigator.vibrate([10])
 		vol.toggleSinkInputMute(id)
-	}
-
-	const throttledHandler = useCallback(
-		throttle((outputName: string, volume: number[]) => {
-			handleSinkVolumeChange(outputName, volume).send()
-		}, 500),
-		[],
-	)
-
-	const handleChange = (outputName: string) => (volume: number[]) => {
-		handleSinkVolumeChange(outputName, volume).optimistic()
-
-		console.log('change')
-		throttledHandler(outputName, volume)
 	}
 
 	return (
@@ -53,8 +64,8 @@ export const ControllerOutput: React.FC = () => {
 						volume={output.volume}
 						label={output.label}
 						onMuteChange={handleSinkMuteToggle(output.name)}
-						onValueChange={handleChange(output.name)}
-						onValueCommit={volume => handleSinkVolumeChange(output.name, volume).send()}
+						onValueChange={volume => handleSinkVolumeChange(output.name, volume)}
+						onValueCommit={volume => sinkVolume(output.name, volume).send()}
 					>
 						{vol.volStatus?.apps?.map(
 							app =>
@@ -72,8 +83,8 @@ export const ControllerOutput: React.FC = () => {
 											label={app.label}
 											volume={app.volume}
 											onMuteChange={handleSinkInputMuteToggle(app.id)}
-											onValueChange={volume => handleSinkInputVolumeChange(app.id, volume).optimistic()}
-											onValueCommit={volume => handleSinkInputVolumeChange(app.id, volume).send()}
+											onValueChange={volume => handleSinkInputVolumeChange(app.id, volume)}
+											onValueCommit={volume => sinkInputVolume(app.id, volume).send()}
 										/>
 									</Fragment>
 								),
---
+++ src/utils/use-throttled-callback.tsx
@@ -0,0 +1,22 @@
+import throttle from 'lodash.throttle'
+import { useCallback } from 'react'
+
+/**
+ * @example
+ *
+ * // In component:
+ * const throttledVolumeChange = useThrottledCallback(
+ *   (outputName: string, volume: number[]) => {
+ *     handleSinkVolumeChange(outputName, volume).send()
+ *   },
+ *   100
+ * )
+ *
+ * const handleChange = (outputName: string) => (volume: number[]) => {
+ *   handleSinkVolumeChange(outputName, volume).optimistic()
+ *   throttledVolumeChange(outputName, volume)
+ * }
+ */
+export function useThrottledCallback<T extends (...args: any[]) => any>(callback: T, delay: number) {
+	return useCallback(throttle(callback, delay), [callback, delay])
+}
