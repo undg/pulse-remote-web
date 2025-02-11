4ba413d1a8add04349e7eca5b532ef1d312cb7a9
undg <und3rdg@gmail.com>
2025-02-11 14:35:08 +0000

    minor change to header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,9 @@
 import type { FC, PropsWithChildren } from 'react'
 import { useTheme } from '../config/use-theme'
-import { H1 } from '../primitives/typography'
 import { cn } from '../utils/cn'
 import { TopNav } from './top-nav'
+import { H2 } from '../primitives/typography'
+import { dict } from '../dict'
 
 export const Layout: FC<PropsWithChildren<{ header?: string }>> = props => {
 	const [theme] = useTheme()
@@ -15,8 +16,11 @@ export const Layout: FC<PropsWithChildren<{ header?: string }>> = props => {
 				theme === 'light' && 'light',
 			)}
 		>
-			<div className='w-full max-w-screen-lg bg-muted p-8 pt-12'>
-				<H1>{props.header ?? ''}</H1>
+			<div className='w-full max-w-screen-lg bg-muted p-8 pt-0'>
+				<div className='flex justify-between'>
+					<H2>{props.header ?? ''}</H2>
+					<H2>{dict.appName}</H2>
+				</div>
 				<TopNav />
 				<main>{props.children}</main>
 			</div>
--- src/dict.ts
+++ src/dict.ts
@@ -1,7 +1,7 @@
 export const dict = {
 	appName: 'Pulse Remote',
-	headerInput: 'Pulse Remote: Input',
-	headerOutput: 'Pulse Remote: Output',
-	headerAbout: 'Pulse Remote: About',
-	headerConfig: 'Pulse Remote: Config',
+	headerInput: 'Input',
+	headerOutput: 'Output',
+	headerAbout: 'About',
+	headerConfig: 'Config',
 } as const
