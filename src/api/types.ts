9114d9bd41aebd6f23488d9f531f501d2b11ba1e
undg <und3rdg@gmail.com>
2025-02-13 21:09:17 +0000

    extract all union types to reuse them.

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,5 +1,5 @@
 import { Action } from '../generated/message'
-import { PrapiStatus } from '../generated/status'
+import { App, Output, PrapiStatus, Source } from '../generated/status'
 
 // @TODO (undg) 2024-09-19: generate those types on the BE or generate them from GetSchema API provided by the server.
 type ActionIn = 'GetStatus'
@@ -13,40 +13,77 @@ export type IncomingMessage = {
 	error?: string
 }
 
+// [Status] Get composed informations about all sinks, sources, inputs and build
+//////////////////////////////////////////////////////////////////
+export type MessageGetStatus = {
+	action: Action.GetStatus
+}
+
+// [BuildInfo]
+//////////////////////////////////////////////////////////////////
+export type MessageGetBuildInfo = {
+	action: Action.GetBuildInfo
+}
+
+// [SINK] SINKS, e.g. Speakers
+//////////////////////////////////////////////////////////////////
+export type MessageSetSinkVolume = {
+	action: Action.SetSinkVolume
+	payload: { name: Output['name']; volume: Output['volume'] }
+}
+
+export type MessageSetSinkMuted = {
+	action: Action.SetSinkMuted
+	payload: { name: Output['name']; muted: Output['muted'] }
+}
+
+// [SinkInput] APPs playing audio
+//////////////////////////////////////////////////////////////////
+export type MessageSetSinkInputVolume = {
+	action: Action.SetSinkInputVolume
+	payload: { id: App['id']; volume: App['volume'] }
+}
+
+export type MessageSetSinkInputMuted = {
+	action: Action.SetSinkInputMuted
+	payload: { id: App['id']; muted: App['muted'] }
+}
+
+export type MessageMoveSinkInput = {
+	action: Action.MoveSinkInput
+	payload: { name: Output['name']; id: App['id'] }
+}
+
+// [Source] Microphones and recording devices
+//////////////////////////////////////////////////////////////////
+export type MessageSetSourceVolume = {
+	action: Action.SetSourceVolume
+	payload: { name: Source['name']; volume: Source['volume'] }
+}
+
+export type MessageSetSourceMuted = {
+	action: Action.SetSourceMuted
+	payload: { name: Source['name']; muted: Source['muted'] }
+}
+
 // @TODO (undg) 2025-02-10: generate it from go-prapi/json/json.go `AvailableCommands`
 /** Message send to the server via websocket */
 export type Message =
 	// Get composed informations about all sinks, sources, inputs and build
-	| { action: Action.GetStatus }
+	| MessageGetStatus
 
 	// Metadata about build
-	| { action: Action.GetBuildInfo }
+	| MessageGetBuildInfo
 
 	// SINKS, e.g. Speakers
-	| {
-			action: Action.SetSinkVolume
-			payload: { name: string; volume: number }
-	  }
-	| {
-			action: Action.SetSinkMuted
-			payload: { name: string; muted: boolean }
-	  }
+	| MessageSetSinkVolume
+	| MessageSetSinkMuted
 
 	// Apps playing audio
-	| {
-			action: Action.SetSinkInputVolume
-			payload: { id: number; volume: number }
-	  }
-	| {
-			action: Action.SetSinkInputMuted
-			payload: { id: number; muted: boolean }
-	  }
+	| MessageSetSinkInputVolume
+	| MessageSetSinkInputMuted
+	| MessageMoveSinkInput
+
 	// SOURCES, Microphone
-	| {
-			action: Action.SetSourceVolume
-			payload: { name: string; volume: number }
-	  }
-	| {
-			action: Action.SetSourceMuted
-			payload: { name: string; muted: boolean }
-	  }
+	| MessageSetSourceVolume
+	| MessageSetSourceMuted
